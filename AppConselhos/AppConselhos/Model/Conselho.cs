f01fc0627289833d92afff4c96069831b156addd
Usuario <Usuario@CPD03-M04>
2022-10-18 12:24:05 -0300

    Adicionar arquivos de projeto.

---
+++ AppConselhos/AppConselhos/Model/Conselho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhos.Model
+{
+    class Conselho
+    {
+        public string ID { get; set; }
+        public string Conselhos { get; set; }
+
+        public Conselho()
+        {
+            this.ID = " ";
+            this.Conselhos = " ";
+        }
+    }
+}
---
+++ AppConselhos/AppConselhos/Services/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppConselhos.Model;
+
+namespace AppConselhos.Services
+{
+   
+        class DataService
+        {
+            public static async Task<Conselhos> GetVida(string ID)
+            {
+
+                string queryString = "https://api.adviceslip.com/advice" + ID + "&unitsm=metric" + "&appid=" + appId;
+                dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+                if (resultado["weather"] != null)
+                {
+                    Conselho Vida = new Conselho();
+                    Vida.ID = (string)resultado["numeric"];
+                    Conselho Vida = new Conselho();
+                    Conselho. Vida = (string)resultado["name"];
+        
+            }
+        }
+}
